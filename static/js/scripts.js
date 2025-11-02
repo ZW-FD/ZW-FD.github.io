567ac7840a32fa9fe3f45d880b96c648e377231d
ZW-FD <60431225+ZW-FD@users.noreply.github.com>
2025-11-02 22:17:47 +0800

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -2,7 +2,7 @@
 
 const content_dir = 'contents/'
 const config_file = 'config.yml'
-const section_names = ['home', 'Working Paper', 'awards']
+const section_names = ['home', 'working paper', 'awards']
 
 
 window.addEventListener('DOMContentLoaded', event => {
